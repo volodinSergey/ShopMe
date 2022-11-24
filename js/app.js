10743c562eb4a8e82bb9ab964f339c359470cd8a
Volodin Sergey <volodin7ergey@icloud.com>
2022-11-25 02:10:19 +0300

    Finalized light theme css file

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ import ProductsList from './components/ProductsList/index.js'
 import Categories from './components/Categories/index.js'
 import Cart from './components/Cart/index.js'
 
-ProductsList()
-Categories()
-Cart()
+const App = async () => {
+    await ProductsList()
+    await Categories()
+    await Cart()
+}
+
+App()

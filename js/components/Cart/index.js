2af45f56674001986194e88b87ebc0fe3fcb5706
Volodin Sergey <volodin7ergey@icloud.com>
2022-11-24 17:06:01 +0300

    Finished cart markdown and basic styles

--- js/components/Cart/index.js
+++ js/components/Cart/index.js
@@ -1,11 +1,25 @@
 const Cart = () => {
     const cartStatusButton = document.querySelector('.cart-status__button')
+    const closeCartButton = document.querySelector('.cart__top-close-cart-button')
+    const overlay = document.querySelector('.overlay')
+    const cart = document.querySelector('.cart')
+
+    const isClassContains = (element, classToCheck) => element.classList.contains(classToCheck)
 
     cartStatusButton.addEventListener('click', (e) => {
-        if (!e.target.classList.contains('cart-status__button-cart')) return
+        if (isClassContains(e.target, 'cart-status__button-cart') || isClassContains(e.target, 'cart-status__button')) {
+            overlay.classList.add('overlay-active')
+            cart.classList.add('cart-active')
+        }
 
-        document.querySelector('.overlay').classList.add('overlay-active')
-        document.querySelector('.cart').classList.add('cart-active')
+        return
+    })
+
+    closeCartButton.addEventListener('click', (e) => {
+        if (isClassContains(e.target, 'close-cart-icon') || isClassContains(e.target, 'cart__top-close-cart-button')) {
+            overlay.classList.remove('overlay-active')
+            cart.classList.remove('cart-active')
+        }
     })
 }
 

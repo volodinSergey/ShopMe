f37e9d82111ca6d26b6e922d0876191b14095ff0
Volodin Sergey <volodin7ergey@icloud.com>
2022-11-25 13:23:01 +0300

    Refactor/Organized LocalStorage service for better separation of concern

--- js/services/Cart.service.js
+++ js/services/Cart.service.js
@@ -1,26 +1,28 @@
+import LocalStorageService from "../shared/services/LocalStorage.service.js"
+
 const CartService = {
-    getAll: function () {
-        const cartItems = JSON.parse(localStorage.getItem('cart'))
+    getAll: () => {
+        const cartItems = LocalStorageService.get('cart')
 
         return cartItems
     },
 
     add: newProduct => {
-        const cart = JSON.parse(localStorage.getItem('cart'))
+        const cart = LocalStorageService.get('cart')
 
         cart.push(newProduct)
 
-        localStorage.setItem('cart', JSON.stringify(cart))
+        LocalStorageService.set('cart', cart)
     },
 
     delete: id => {
-        let cart = JSON.parse(localStorage.getItem('cart'))
+        let cart = LocalStorageService.get('cart')
 
         const cartWithoutDeletedProduct = cart.filter(item => item.id !== id)
 
         cart = [...cartWithoutDeletedProduct]
 
-        localStorage.setItem('cart', JSON.stringify(cart))
+        LocalStorageService.set('cart', cart)
     }
 }
 
